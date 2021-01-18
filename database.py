b48f97a3a54af5b2df1f98fd62352336abd3e1f9
Mert Buran <buranme@gmail.com>
2021-01-18 21:36:13 +0300

    h

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
-
 import psycopg2 as db
 
-url  = "dbname='jobApplicationDatabase' user='postgres' host='localhost' password='1234'"
+url  = "dbname='jobApplicationDatabase' user='postgres' host='localhost' password='4041'"
 
 def create_tables():
-    #temp_url = "dbname='db' user='postgres' host='localhost' password='1234'"
     with open("db.sql","r") as sql:
         temp = sql.readlines()
     tmp = ""
