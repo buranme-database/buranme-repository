c00f24af619fd25073c49e42262c4f89628bf63e
Mert Buran <buranme@gmail.com>
2021-01-18 21:17:04 +0300

    test

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
     
+@app.route("/profile")
+def home():
+    return render_template("profile.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
