05e12d69a52d7efaa45b5b05493f63597473828f
Mert Buran <buranme@gmail.com>
2021-01-18 21:58:11 +0300

    h

--- server.py
+++ server.py
@@ -10,5 +10,9 @@ def home():
 def profile():
     return render_template("profile.html")
     
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
