713b07effa93e62aabed9ec08b04fd06879c9954
Mert Buran <buranme@gmail.com>
2021-01-20 05:54:06 +0300

    final push

--- dbinit.py
+++ dbinit.py
@@ -1,22 +1,115 @@
 import os
 import sys
-
+import json
 import psycopg2 as dbapi2
 
 
-INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
-]
+url  = "user='postgres' dbname='jobApplicationDatabase' host='localhost' password='4041'"
 
 
-def initialize(url):
-    with dbapi2.connect(url) as connection:
-        cursor = connection.cursor()
-        for statement in INIT_STATEMENTS:
-            cursor.execute(statement)
+def student_signup(password,username,name):
+    flag=False
+    id=0
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+    statement = """INSERT INTO public."Student"(
+	"Password", "Username", "Name")
+	VALUES ('{}', '{}', '{}') RETURNING "ID";""".format(password,username,name)
+    try:
+        cursor.execute(statement)
+        id = cursor.fetchone()
+        connection.commit()
+        id = int(id[0])
+        flag=True
+    except Exception as err:
+        if (err.pgcode == "23505"):
+            print("this email is already in use")
+            id=-1
+            flag=False
+    finally:
         cursor.close()
+        connection.close()
+    return (flag,id)
 
+def company_signup(password,username,name):
+    flag=False
+    id=0
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+    statement = """INSERT INTO public."Company"(
+	"Password", "Username", "Name")
+	VALUES ('{}', '{}', '{}') RETURNING "ID";""".format(password,username,name)
+    try:
+        cursor.execute(statement)
+        id = cursor.fetchone()
+        connection.commit()
+        id = int(id[0])
+        flag=True
+    except Exception as err:
+        if (err.pgcode == "23505"):
+            print("this email is already in use")
+            id=-1
+            flag=False
+    finally:
+        cursor.close()
+        connection.close()
+    return (flag,id)
+
+def student_login(username,password):
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+    statement = """SELECT "ID" FROM public."Student" where "Username" = '{}' and "Password"='{}';""".format(username,password)
+
+    cursor.execute(statement)
+    result = cursor.fetchone()
+    if result:
+        return (True,int(result[0]))
+    
+    statement = """SELECT "ID" FROM public."Student" where "Username" = '{}' ;""".format(username)
+    if result:
+        print('wrong password')
+        return (False,int(result[0]))
+    print("user doesn't exist")
+    return(False,-1)
+
+def company_login(username,password):
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+    statement = """SELECT "ID" FROM public."Company" where "Username" = '{}' and "Password"='{}';""".format(username,password)
+    cursor.execute(statement)
+    result = cursor.fetchone()
+    if result:
+        return (True,int(result[0]))
+    statement = """SELECT "ID" FROM public."Company" where "Username" = '{}' ;""".format(username)
+    if result:
+        print('wrong password')
+        return (False,int(result[0]))
+    print("Company doesn't exist")
+    return(False,-1)
+
+def add_advert(company_id, name):
+    connection = dbapi2.connect(url)
+    cursor = connection.cursor()
+    statement = """INSERT INTO public."Advert" ("Company ID","Name")
+    VALUES ( '{}','{}') RETURNING "ID";""".format(company_id, name)
+    try:
+        cursor.execute(statement)
+        id = cursor.fetchone()
+        id = id[0]
+        connection.commit()
+        flag=True
+        cursor.close()
+        connection.close()
+        return (flag,id)
+    except Exception as err:
+        print_psycopg2_exception(err)
+        flag=False
+        cursor.close()
+        connection.close()
+        return (flag,-1)
+    finally:
+        cursor.close()
+        connection.close()
 
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template, request, jsonify
+import dbinit   
 
 app = Flask(__name__)
 
@@ -6,13 +7,92 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
     
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/profileCompany")
+def profileCompany():
+    return render_template("profileCompany.html")
+    
+@app.route("/profileStudent")
+def profileStudent():
+    return render_template("profileStudent.html")
     
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/mainPageStudent")
+def mainPageStudent():
+    return render_template("mainPageStudent.html")
     
+@app.route("/mainPageCompany")
+def mainPageCompany():
+    return render_template("mainPageCompany.html")
+
+@app.route('/ssignup',methods=['POST'])
+def ssignup():
+    password = request.args.get('password')
+    username = request.args.get('username')
+    name = request.args.get('name')
+
+    res = dbinit.student_signup(password,username,name)
+
+    ret = {
+        "flag":res[0],
+        "id":res[1]
+    }
+    return jsonify(ret)
+
+@app.route('/csignup',methods=['POST'])
+def csignup():
+    password = request.args.get('password')
+    username = request.args.get('username')
+    name = request.args.get('name')
+
+    res = dbinit.company_signup(password,username,name)
+
+    ret = {
+        "flag":res[0],
+        "id":res[1]
+    }
+    return jsonify(ret)
+    
+@app.route('/clogin',methods=['POST'])
+def clogin():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    res = dbinit.company_login(username,password)
+    ret = {
+        "flag":res[0],
+        "id":res[1]
+    }
+    return jsonify(ret)
+
+@app.route('/slogin',methods=['POST'])
+def slogin():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    res = dbinit.student_login(username,password)
+    ret = {
+        "flag":res[0],
+        "id":res[1]
+    }
+    return jsonify(ret)
+
+@app.route('/addbranch',methods=['POST'])
+def addbranch():
+    company_id = request.args.get('company_id')
+    name = request.args.get('name')
+
+    res = dbinit.add_branch(company_id,name)
+
+    ret = {
+        "flag":res[0],
+        "id":res[1]
+    }
+    return jsonify(ret)
+
 if __name__ == "__main__":
     app.run(debug=True)
